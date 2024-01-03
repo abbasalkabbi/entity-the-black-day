9d7504d2d6ec2dbc297b64d3d15368b1ecc5203f
abbas alkabbi <abbasalkabbi2003@gmail.com>
2024-01-03 17:31:50 +0300

    "gallery "

---
+++ app.js
@@ -0,0 +1,41 @@
+const div_show_img=document.querySelector(".gallery .show-img")
+const div_next=document.querySelector(".gallery .next")
+const div_pref=document.querySelector(".gallery .previous")
+div_show_img.style.display='none'
+
+
+function div_img(url,action){
+    if(action=="hide"){
+        div_show_img.style.display='none'
+    }
+    if(action=='show'){
+        div_show_img.style.backgroundImage=`url(./assets/${url})`
+        div_show_img.style.display='block'
+    }
+    // mext
+    if(action=="next"){
+        console.log(div_show_img.style.backgroundImage)
+        switch (div_show_img.style.backgroundImage) {
+            case 'url("./assets/main_img-1.jpg")':
+                div_show_img.style.backgroundImage=`url(./assets/main_img-2.jpg)`
+                break;
+            
+            case `url("./assets/main_img-2.jpg")`:
+                div_show_img.style.backgroundImage=`url(./assets/main_img-3.jpg)`
+                break;
+            case `url("./assets/main_img-3.jpg")`:
+                div_show_img.style.backgroundImage=`url(./assets/main_img-4.jpg)`
+                break;
+            case `url("./assets/main_img-4.jpg")`:
+                div_show_img.style.backgroundImage=`url(./assets/main_img-5.jpg)`
+                break;
+            case `url("./assets/main_img-5.jpg")`:
+                div_show_img.style.backgroundImage=`url(./assets/main_img-1.jpg)`
+                break;
+    }
+    }
+    // end next
+}
+function show(url,action){
+div_img(url,action)
+}
